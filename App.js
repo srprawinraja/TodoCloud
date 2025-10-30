1a57041d89b5b5227d7c484e861ca549cbba3b36
SR PRAWIN RAJA <srprawinraja@gmail.com>
2025-10-30 21:38:41 +0530

    added authentication feature

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import colors from './src/theme/colors';
 import AppNavigator from './src/navigation/AppNavigator';
 import { NavigationContainer } from '@react-navigation/native';
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 
 export default function App() {
-  return <>
+  return <ClerkProvider tokenCache={tokenCache}>
+
     <StatusBar barStyle="dark-content" backgroundColor= 'white'/>
     <NavigationContainer>
         <AppNavigator/>
       </NavigationContainer>
-  </>
+  </ClerkProvider>
+  
 }
--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -6,14 +6,25 @@ import SignInScreen from '../screens/SignInScreen/index';
 import VerifyScreen from '../screens/VerifyScreen/index';
 import { JSX } from 'react';
 import TestScreen from '../screens/TestScreen';
+import { useAuth } from '@clerk/clerk-expo'
 
 
 
-const Stack:any = createNativeStackNavigator();
 
 export default function AppNavigator() {
+  const Stack:any = createNativeStackNavigator();
+const { isSignedIn, isLoaded } = useAuth()
+  const getIntialScreen = ()=>{
+    if (!isLoaded || !isSignedIn) {
+        return
+    }
+    if(isSignedIn){
+      return "Todo"
+    } 
+    return "SignIn"
+  }
   return (
-    <Stack.Navigator initialRouteName="SignIn" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName={getIntialScreen()} screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Todo" component={TodoScreen} />
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
